51d76ce6a43762d1fd6979a342fb1e14f1eb4a83
(Deivide <(deividemaciel2@gmail.com)>
2023-10-19 18:18:23 -0300

    Feat: Adicionado o form para selecionar um arquivo.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css'
 
 function App() {
+
+
   return (
     <div className="App">
       <div className="Intro">
         <h1> Json Tree Viewer </h1>
         <p>Simple JSON Viewer that runs completely on-client. No data exchange</p>
-        <button>Load JSON</button>
+
+        <form className="file-form" onsubmit="">
+          <label for="input-file">Load Json</label>
+          <input type="file" name="file" id="input-file" />
+          
+          <button type="submit" className="submit-button">
+            Confirm
+          </button>
+        </form>
       </div>
     </div>
   )
